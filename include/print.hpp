fc053b5671b4fe256b11f52bac8e1611fe27ac21
AlexDeveloper24 <alexchubukov12345@gmail.com>
2018-05-24 07:44:55 +0000

    added documentation

--- include/print.hpp
+++
@@ -1,6 +0,0 @@
-#include <string>
-#include <fstream>
-#include <iostream>
-
-void print(const std::string& text, std::ostream& out = std::cout); //вывод в стандартный поток
-void print(const std::string& text, std::ofstream& out); // вывод в файл
